d22dc7c060ad39ae5ee06d9a2ce20e81c0ac3176
Harrison Kong <harrisonkong@skyroute66.com>
2020-01-11 15:38:11 -0800

    Initial Commit

---
+++ HKKBMDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HKKBMDemo
+//
+//  Created by Harrison Kong on 1/11/20.
+//  Copyright © 2020 skyRoute66. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
