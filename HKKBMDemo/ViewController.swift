13210259117b7ab4ba2ed573c1e4a0cc8b893a88
Harrison Kong <harrisonkong@skyroute66.com>
2020-01-13 16:50:13 -0800

    All gesture recognizers

--- HKKBMDemo/ViewController.swift
+++ HKKBMDemo/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
@@ -14,7 +15,9 @@ class ViewController: UIViewController {
     // MARK: -
 
     private var heightConstraint : NSLayoutConstraint?
-
+    var kbManager : HKUIKeyboardManagerScrollable?
+    var bellPlayer:AVAudioPlayer? = nil
+    
     // MARK: - IB Outlets
     // MARK: -
     
@@ -24,13 +27,88 @@ class ViewController: UIViewController {
     @IBOutlet weak var segueSwitch: UISwitch!
     @IBOutlet weak var deviceRotationSwitch: UISwitch!
 
+    @IBOutlet weak var boat: UIImageView!
+    @IBOutlet weak var boatBox: UIView!
+    
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var journalTextView: UITextView!
-    
-    var kbManager : HKUIKeyboardManagerScrollable?
-    
+            
     // MARK: - IB Actions
     // MARK: -
+    
+    @IBAction func deviceRotationToggled(_ sender: UISwitch) {
+        kbManager?.dismissDuringDeviceRotation = sender.isOn
+        
+        if !sender.isOn && !segueSwitch.isOn {
+            segueSwitch.isOn = true
+        }
+    }
+
+    @IBAction func handlePan(recognizer:UIPanGestureRecognizer) {
+        let translation = recognizer.translation(in: boatBox)
+                
+        if let boat = recognizer.view {
+            
+            var newX = boat.center.x + translation.x
+            
+            if newX < boat.frame.width / 2.0 {
+                newX = boat.frame.width / 2.0
+            }
+
+            if newX > boatBox.frame.width - boat.frame.width / 2.0 {
+                newX = boatBox.frame.width - boat.frame.width / 2.0
+            }
+
+            var newY = boat.center.y + translation.y
+            
+            if newY < boat.frame.height / 2.0 {
+                newY = boat.frame.height / 2.0
+            }
+
+            if newY > boatBox.frame.height - boat.frame.height / 2.0 {
+                newY = boatBox.frame.height - boat.frame.height / 2.0
+            }
+            
+            boat.center = CGPoint(x: newX, y: newY)
+        }
+        recognizer.setTranslation(CGPoint.zero, in: view)
+    }
+
+    @IBAction func handlePinch(recognizer : UIPinchGestureRecognizer) {
+        
+        if let helm = recognizer.view {
+          helm.transform = helm.transform.scaledBy(x: recognizer.scale, y: recognizer.scale)
+          recognizer.scale = 1
+        }
+    }
+
+    @IBAction func handleRotate(recognizer : UIRotationGestureRecognizer) {
+        
+        if let helm = recognizer.view {
+          helm.transform = helm.transform.rotated(by: recognizer.rotation)
+          recognizer.rotation = 0
+        }
+    }
+ 
+    @IBAction func handleTap(recognizer: UITapGestureRecognizer) {
+        self.bellPlayer?.play()
+    }
+    
+    @IBAction func panGesturesToggled(_ sender: UISwitch) {
+        kbManager?.dismissOnPanGestures = sender.isOn
+    }
+    
+    @IBAction func pinchGesturesToggle(_ sender: UISwitch) {
+        kbManager?.dismissOnPinchGestures = sender.isOn
+    }
+    
+    @IBAction func rotationGesturesToggled(_ sender: UISwitch) {
+        kbManager?.dismissOnRotationGestures = sender.isOn
+    }
+    
+    @IBAction func scrollActiveFieldToggled(_ sender: UISwitch) {
+        kbManager?.keepActiveFieldInView = sender.isOn
+    }
     
     @IBAction func segueToggled(_ sender: UISwitch) {
         kbManager?.dismissDuringSegue = sender.isOn
@@ -40,32 +118,8 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func deviceRotationToggled(_ sender: UISwitch) {
-        kbManager?.dismissDuringDeviceRotation = sender.isOn
-        
-        if !sender.isOn && !segueSwitch.isOn {
-            segueSwitch.isOn = true
-        }
-    }
-    
     @IBAction func tapGesturesToggled(_ sender: UISwitch) {
         kbManager?.dismissOnTapGestures = sender.isOn
-    }
-    
-    @IBAction func panGesturesToggled(_ sender: UISwitch) {
-        kbManager?.dismissOnPanGestures = sender.isOn
-    }
-    
-    @IBAction func rotationGesturesToggled(_ sender: UISwitch) {
-        kbManager?.dismissOnRotationGestures = sender.isOn
-    }
-    
-    @IBAction func pinchGesturesToggle(_ sender: UISwitch) {
-        kbManager?.dismissOnPinchGestures = sender.isOn
-    }
-    
-    @IBAction func scrollActiveFieldToggled(_ sender: UISwitch) {
-        kbManager?.keepActiveFieldInView = sender.isOn
     }
     
     // MARK: - Lifecycle
@@ -74,8 +128,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        containerView.backgroundColor = .red
-        scrollView.backgroundColor = .green
+        containerView.backgroundColor = .orange
+        scrollView.backgroundColor = .blue
+
+        // load the bell sound
+        let url = Bundle.main.url(forResource: "ship-bell", withExtension: "mp3")
+        var player = AVAudioPlayer()
+        do {
+          try player = AVAudioPlayer(contentsOf: url!)
+          player.prepareToPlay()
+        } catch {
+          print("Error loading sound \(url!): \(error.localizedDescription)")
+        }
+        bellPlayer = player
 
         kbManager = HKUIKeyboardManagerScrollable(ownerView: scrollView, outermostView: view)
         kbManager?.dismissDuringDeviceRotation = false
@@ -102,5 +167,6 @@ class ViewController: UIViewController {
         
         kbManager?.viewWillTransition()
     }
+    
 }
 
